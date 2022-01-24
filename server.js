cce14c0986048b439a8c98b3a1c537a391883c2c
Marcos Manresa <88654938+MarcosManresa@users.noreply.github.com>
2022-01-23 21:35:56 -0500

    Added read me

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const goose = require('mongoose');
+
+const apper = express();
+const PORT = process.env.PORT || 3001;
+
+apper.use(express.json());
+apper.use(express.urlencoded({ extended: true }));
+apper.use(require('./routes'));
+
+goose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+goose.set('debug', true);
+
+apper.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
